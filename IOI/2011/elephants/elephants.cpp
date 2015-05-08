83dd39706cfd794b22a9af623c51e5056950043a
Alex Pizarro <alex.pizarro.j@gmail.com>
2015-05-08 01:35:41 -0400

    elephants style fixes, IOI 2011

--- IOI/2011/elephants/elephants.cpp
+++ IOI/2011/elephants/elephants.cpp
@@ -3,21 +3,22 @@ using namespace std;
 
 struct Elephant {
   Elephant(int pos) : x(pos) {}
-  int id, x, cneeded, ccover;
-  int be, en;
+  Elephant(int pos, int ident) : x(pos), id(ident) {}
+  int x, id, be, en;
+  int cneeded, ccover;
 };
 
 bool operator<(const Elephant& lhs, const Elephant& rhs) {
   return lhs.x < rhs.x;
 }
 
-const int inf = 1000000007;
+static const int inf = 1000000007;
 static vector<int> epos;
 static int global_N, global_L, n_buckets, buckets_span, update_cnt;
 static vector<vector<Elephant> > buckets;
 static vector<pair<int, int> > buckets_int;
 
-void fill_bucket_data(vector<Elephant>& v) {
+static void fill_bucket_data(vector<Elephant>& v) {
   const int N = v.size();
   int next = N;
   for (int cur = N - 1; cur >= 0; cur--) {
@@ -33,69 +34,81 @@ void fill_bucket_data(vector<Elephant>& v) {
   }
 }
 
-void build_buckets(const vector<pair<int, int> >& X) {
+static void build_buckets(const vector<pair<int, int> >& X) {
   int covered = 0;
   buckets.assign(n_buckets, vector<Elephant>());
   buckets_int.resize(n_buckets);
   for (int i = 0; i < n_buckets; i++) {
-    const int lb = i * buckets_span, ub = min(lb + buckets_span, global_N);
+    const int lb = i * buckets_span;
+    const int ub = min(lb + buckets_span, global_N);
     for (int j = lb; j < ub; j++) {
-      buckets[i].push_back(Elephant(X[j].first));
-      buckets[i].back().id = X[j].second;
+      buckets[i].push_back(Elephant(X[j].first, X[j].second));
       epos[X[j].second] = i;
     }
 
-    buckets_int[i].first = buckets_int[i].second = covered;
-    if (i == n_buckets - 1)
+    buckets_int[i].first = covered;
+    buckets_int[i].second = covered;
+    if (i == n_buckets - 1) {
       buckets_int[i].second = inf;
-    else if (buckets[i].size() > 0)
+    } else if (buckets[i].size() > 0) {
       buckets_int[i].second = buckets[i].back().x;
+    }
     covered = buckets_int[i].second;
 
     fill_bucket_data(buckets[i]);
   }
 }
 
-void rebuild_buckets() {
+static void rebuild_buckets() {
   vector<pair<int, int> > X;
-  for (int i = 0; i < n_buckets; i++)
-    for (int j = 0; j < (int)buckets[i].size(); j++)
+  for (int i = 0; i < n_buckets; i++) {
+    for (int j = 0; j < (int)buckets[i].size(); j++) {
       X.push_back(make_pair(buckets[i][j].x, buckets[i][j].id));
+    }
+  }
   assert((int)X.size() == global_N);
   build_buckets(X);
 }
 
-int get_min_cameras() {
+static int get_min_cameras() {
   int ans = 0, covered = -1;
   for (int i = 0; i < (int)buckets.size(); i++) {
     const vector<Elephant>& b = buckets[i];
     vector<Elephant>::const_iterator it =
         upper_bound(b.begin(), b.end(), Elephant(covered));
-    if (it == b.end()) continue;
+    if (it == b.end()) {
+      continue;
+    }
+
     ans += it->cneeded;
     covered = it->ccover;
-    // cout << "Taken elephant = " << it->id <<
-    //  ", covers up to " << covered << " using " << it->cneeded <<
-    //  " cameras" << endl;
   }
   return ans;
 }
 
 void init(int N, int L, int X[]) {
-  global_N = N, global_L = L;
+  global_N = N;
+  global_L = L;
   n_buckets = 1;
-  while (n_buckets * n_buckets < N) n_buckets += 1;
+
+  while (n_buckets * n_buckets < N) {
+    n_buckets += 1;
+  }
+
   buckets_span = n_buckets;
   update_cnt = 0;
   epos.resize(N);
 
   vector<pair<int, int> > XX(N);
-  for (int i = 0; i < N; i++) XX[i] = make_pair(X[i], i);
+  for (int i = 0; i < N; i++) {
+    XX[i] = make_pair(X[i], i);
+  }
   build_buckets(XX);
 }
 
 int update(int id, int y) {
   int prev_bucket = epos[id], new_bucket = -1;
+
   // Locate the corresponding buckets (affected by the change)
   for (int i = 0; i < n_buckets; i++) {
     const int be = buckets_int[i].first, en = buckets_int[i].second;
@@ -103,6 +116,7 @@ int update(int id, int y) {
   }
   epos[id] = new_bucket;
   assert(new_bucket != -1);
+
   // Remove the previous elephant
   bool removed = false;
   for (int i = 0; i < (int)buckets[prev_bucket].size(); i++) {
@@ -113,15 +127,16 @@ int update(int id, int y) {
     }
   }
   assert(removed);
+
   // Insert the new elephant
   vector<Elephant>::iterator insert_it = upper_bound(
       buckets[new_bucket].begin(), buckets[new_bucket].end(), Elephant(y));
-  Elephant e(y);
-  e.id = id;
-  buckets[new_bucket].insert(insert_it, e);
+  buckets[new_bucket].insert(insert_it, Elephant(y, id));
+
   // Update the buckets
   fill_bucket_data(buckets[prev_bucket]);
   if (prev_bucket != new_bucket) fill_bucket_data(buckets[new_bucket]);
+
   // Calculate answer + Optimize structure
   int ans = get_min_cameras();
   update_cnt += 1;
