d83fede365251455b4943ddc104432b9226b98b5
Alex Pizarro <alex.pizarro.j@gmail.com>
2017-10-07 16:09:16 -0400

    Update template: CodeForces to just 'C++'

---
+++ templates/cpp/G.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int inf = 1000000007;
+using ii = pair<int, int>;
+using iii = pair<int, ii>;
+using vi = vector<int>;
+using vii = vector<ii>;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  
+  //
+  
+  return 0;
+}
