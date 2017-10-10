2ca89a0931d22a79d63407d74cd5c86ca63d1f2a
Alex Pizarro <alex.pizarro.j@gmail.com>
2017-10-10 13:50:47 -0400

    Please @Garciat the Fucker

--- src/uva/403.cpp
+++ src/uva/403.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 using namespace std;
 
-const vector<string> c5FontArt{
+const vector<string> kLargeFontArt{
     ".***..", "*...*.", "*****.", "*...*.", "*...*.", "****..", "*...*.",
     "****..", "*...*.", "****..", ".****.", "*...*.", "*.....", "*.....",
     ".****.", "****..", "*...*.", "*...*.", "*...*.", "****..", "*****.",
@@ -29,19 +29,22 @@ const vector<string> c5FontArt{
 
 class Page {
  public:
-  Page() { clean(); }
+  Page() { Clean(); }
 
-  void print() {
-    for (auto&& line : grid) cout << line << '\n';
+  void Print() {
+    for (auto&& line : grid_) cout << line << '\n';
     cout << '\n' << string(kColCount, '-') << '\n' << endl;
-    clean();
   }
 
-  void write(const string& text, int row, int col, bool useLargeFont) {
-    if (!useLargeFont) {
-      for (int i = 0; i < (int)text.size(); ++i) {
-        writeOnGrid(row, col + i, text[i]);
-      }
+  void PrintAndClean() {
+    Print();
+    Clean();
+  }
+
+  void Write(const string& text, int row, int col, bool use_large_font) {
+    if (!use_large_font) {
+      for (int i = 0; i < (int)text.size(); ++i)
+        WriteOnGrid(row, col + i, text[i]);
       return;
     }
 
@@ -53,109 +56,129 @@ class Page {
 
       for (int j = 0; j < kLargeFontRowsPerChar; ++j) {
         for (int k = 0; k < kLargeFontColsPerChar; ++k) {
-          int dstRow = row + j;
-          int dstCol = col + i * kLargeFontColsPerChar + k;
-          writeOnGrid(dstRow, dstCol, c5FontArt[j + kFontArtOffset][k]);
+          int dst_row = row + j;
+          int dst_col = col + i * kLargeFontColsPerChar + k;
+          WriteOnGrid(dst_row, dst_col, kLargeFontArt[j + kFontArtOffset][k]);
         }
       }
     }
   }
 
-  void writeLeftJustified(const string& text, int row, bool useLargeFont) {
+  void WriteLeftJustified(const string& text, int row, bool use_large_font) {
     int col = 0;
-    write(text, row, col, useLargeFont);
+    Write(text, row, col, use_large_font);
   }
 
-  void writeRightJustified(const string& text, int row, bool useLargeFont) {
-    int col = kColCount - measureTextHorizontally(text, useLargeFont);
-    write(text, row, col, useLargeFont);
+  void WriteRightJustified(const string& text, int row, bool use_large_font) {
+    int col = kColCount - MeasureTextHorizontally(text, use_large_font);
+    Write(text, row, col, use_large_font);
   }
 
-  void writeCentered(const string& text, int row, bool useLargeFont) {
-    int hlen = measureTextHorizontally(text, useLargeFont);
+  void WriteCentered(const string& text, int row, bool use_large_font) {
+    int text_len = MeasureTextHorizontally(text, use_large_font);
 
-    int col = (kColCount - hlen) / 2;
-    if (hlen % 2 == 1) col = ((kColCount + 2) - (hlen + 1)) / 2;
+    int col = (kColCount - text_len) / 2;
+    if (text_len % 2 == 1) col = ((kColCount + 2) - (text_len + 1)) / 2;
 
-    write(text, row, col, useLargeFont);
+    Write(text, row, col, use_large_font);
   }
 
-  void clean() { grid = vector<string>(kRowCount, string(kColCount, '.')); }
+  void Clean() { grid_ = vector<string>(kRowCount, string(kColCount, '.')); }
 
  private:
   const int kRowCount = 60, kColCount = 60;
   const int kLargeFontRowsPerChar = 5, kLargeFontColsPerChar = 6;
-  vector<string> grid;
+  vector<string> grid_;
 
-  void writeOnGrid(int i, int j, char ch) {
+  void WriteOnGrid(int i, int j, char ch) {
     if (i < 0 || i >= kRowCount) return;
     if (j < 0 || j >= kColCount) return;
     if (ch == '.' || ch == ' ') return;
-    grid[i][j] = ch;
+    grid_[i][j] = ch;
   }
 
-  int measureTextHorizontally(const string& text, bool useLargeFont) {
-    return text.size() * (useLargeFont ? kLargeFontColsPerChar : 1);
+  int MeasureTextHorizontally(const string& text, bool use_large_font) {
+    return text.size() * (use_large_font ? kLargeFontColsPerChar : 1);
   }
 };
 
-bool startsWith(const string& s, const string& prefix) {
-  if (s.size() < prefix.size()) return false;
-  for (int i = 0; i < (int)prefix.size(); ++i)
-    if (s[i] != prefix[i]) return false;
-  return true;
-}
+class PageCommand {
+ public:
+  PageCommand(const string& command_str) {
+    if (StartsWith(command_str, ".EOP")) {
+      command_type_ = "EOP";
 
-vector<string> splitInput(const string& inputLine, int nArgs) {
-  vector<string> result;
+    } else if (StartsWith(command_str, ".P")) {
+      auto args = SplitInput(command_str, 5);
 
-  istringstream iss(inputLine);
-  for (int i = 0; i < nArgs - 1; ++i) {
-    string arg;
-    iss >> arg;
-    result.push_back(arg);
-  }
-
-  iss.ignore(1);
-  string remaining;
-  getline(iss, remaining);
-  result.push_back(remaining);
-
-  return result;
-}
-
-int main() {
-  string inputLine;
-  Page p;
-
-  while (getline(cin, inputLine)) {
-    if (startsWith(inputLine, ".EOP")) {
-      p.print();
-
-    } else if (startsWith(inputLine, ".P")) {
-      auto args = splitInput(inputLine, 5);
-
-      bool useLargeFont = (args[1][1] == '5');
-      int row = atoi(args[2].c_str()) - 1;
-      int col = atoi(args[3].c_str()) - 1;
-      string text = args[4].substr(1, args[4].size() - 2);
-
-      p.write(text, row, col, useLargeFont);
+      command_type_ = "P";
+      use_large_font_ = (args[1][1] == '5');
+      row_ = atoi(args[2].c_str()) - 1;
+      col_ = atoi(args[3].c_str()) - 1;
+      text_ = args[4].substr(1, args[4].size() - 2);
 
     } else {
-      auto args = splitInput(inputLine, 4);
+      auto args = SplitInput(command_str, 4);
 
-      bool useLargeFont = (args[1][1] == '5');
-      int row = atoi(args[2].c_str()) - 1;
-      string text = args[3].substr(1, args[3].size() - 2);
-
-      if (startsWith(inputLine, ".L"))
-        p.writeLeftJustified(text, row, useLargeFont);
-      else if (startsWith(inputLine, ".R"))
-        p.writeRightJustified(text, row, useLargeFont);
-      else if (startsWith(inputLine, ".C"))
-        p.writeCentered(text, row, useLargeFont);
+      command_type_ = args[0].substr(1, args[0].size() - 1);
+      use_large_font_ = (args[1][1] == '5');
+      row_ = atoi(args[2].c_str()) - 1;
+      text_ = args[3].substr(1, args[3].size() - 2);
     }
+  }
+
+  void Execute(Page& page) {
+    if (command_type_ == "EOP")
+      page.PrintAndClean();
+    else if (command_type_ == "P")
+      page.Write(text_, row_, col_, use_large_font_);
+    else if (command_type_ == "L")
+      page.WriteLeftJustified(text_, row_, use_large_font_);
+    else if (command_type_ == "R")
+      page.WriteRightJustified(text_, row_, use_large_font_);
+    else if (command_type_ == "C")
+      page.WriteCentered(text_, row_, use_large_font_);
+  }
+
+ private:
+  string command_type_, text_;
+  bool use_large_font_;
+  int row_, col_;
+
+  bool StartsWith(const string& s, const string& prefix) {
+    if (s.size() < prefix.size()) return false;
+
+    for (int i = 0; i < (int)prefix.size(); ++i)
+      if (s[i] != prefix[i]) return false;
+
+    return true;
+  }
+
+  vector<string> SplitInput(const string& input_line, int num_args) {
+    vector<string> result;
+
+    istringstream iss(input_line);
+    for (int i = 0; i < num_args - 1; ++i) {
+      string arg;
+      iss >> arg;
+      result.push_back(arg);
+    }
+
+    string remaining;
+    iss.ignore(1);
+    getline(iss, remaining);
+    result.push_back(remaining);
+
+    return result;
+  }
+};
+
+int main() {
+  string input_line;
+  Page page;
+
+  while (getline(cin, input_line)) {
+    PageCommand(input_line).Execute(page);
   }
 
   return 0;
