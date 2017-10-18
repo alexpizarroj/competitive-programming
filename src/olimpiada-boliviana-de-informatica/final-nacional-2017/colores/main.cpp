e46dd67e62cda2568361e1686a91e950618f44a6
Alex Pizarro <alex.pizarro.j@gmail.com>
2017-10-18 15:02:46 -0400

    Simplify OBI 2017 - Colores solution

--- src/olimpiada-boliviana-de-informatica/final-nacional-2017/colores/main.cpp
+++ src/olimpiada-boliviana-de-informatica/final-nacional-2017/colores/main.cpp
@@ -5,42 +5,14 @@ using namespace std;
 
 vector<int> prec_color_dominante;
 
-int CalcularColorDominante(int num_colores) {
-  list<int> colores;
-  for (int color = 1; color <= num_colores; ++color)
-    colores.insert(colores.end(), color);
-
-  int num_colores_restantes = num_colores;
-  list<int>::iterator it = colores.begin();
-
-  while (num_colores_restantes > 1) {
-    advance(it, 1);
-    if (it == colores.end()) it = colores.begin();
-
-    it = colores.erase(it);
-    if (it == colores.end()) it = colores.begin();
-
-    num_colores_restantes -= 1;
-  }
-
-  return *colores.begin();
-}
-
-void PrecalcularColoresDominantesSimulando(int num_max_colores) {
+void PrecalcularColoresDominantes(int num_max_colores) {
   prec_color_dominante.resize(num_max_colores + 1);
-
-  for (int num_colores = 1; num_colores <= num_max_colores; ++num_colores)
-    prec_color_dominante[num_colores] = CalcularColorDominante(num_colores);
-}
-
-void PrecalcularColoresDominantesConProgramacionDinamica(int num_max_colores) {
-  prec_color_dominante.resize(num_max_colores + 1);
+  
   prec_color_dominante[1] = 1;
-
-  for (int num_colores = 2; num_colores <= num_max_colores; ++num_colores) {
-    int color = prec_color_dominante[num_colores - 1] + 2;
-    if (prec_color_dominante[num_colores - 1] == num_colores - 1) color = 1;
-    prec_color_dominante[num_colores] = color;
+  for (int c = 2; c <= num_max_colores; ++c) {
+    int color = prec_color_dominante[c - 1] + 2;
+    if (prec_color_dominante[c - 1] == c - 1) color = 1;
+    prec_color_dominante[c] = color;
   }
 }
 
@@ -60,10 +32,9 @@ const int kPintarConFirmaMelchor = -1, kPintarConColorBlanco = 1;
 
 int ObtenerPinturaDeCelda(const int nro_celda) {
   if (nro_celda == 0) return kPintarConFirmaMelchor;
-
+  
   int color = prec_color_dominante[nro_celda];
-  if (!EsColorMersenne(color)) return kPintarConColorBlanco;
-
+  if (!EsColorMersenne(color)) color = kPintarConColorBlanco;
   return color;
 }
 
@@ -72,7 +43,7 @@ int main() {
   cin.tie(NULL);
 
   const int kCantidadMaximaDeCeldas = 1000 * 1000;
-  PrecalcularColoresDominantesConProgramacionDinamica(kCantidadMaximaDeCeldas);
+  PrecalcularColoresDominantes(kCantidadMaximaDeCeldas);
 
   int num_casos;
   cin >> num_casos;
